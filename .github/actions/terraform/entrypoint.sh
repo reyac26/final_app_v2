b44396c364d200df0ec4387d000fc5aa9db6a290
Reya <nereydacastro15@gmail.com>
2026-08-01 18:27:38 -0500

    Modify entrypoint.sh for stage-specific Terraform state
    Updated Terraform entrypoint script to use stage-specific state keys for stage1 and stage2.

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -6,14 +6,15 @@ export STATE_KEY=${INPUT_STATE_KEY}
 export TF_STAGE=${INPUT_TF_STAGE}
 export DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}
 
+export TF_VAR_DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}
+
 if [[ "$TF_STAGE" == "stage1" ]]; then
-    terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
+    terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}-stage1.tfstate"
     terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
     terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
 
 elif [[ "$TF_STAGE" == "stage2" ]]; then
-    terraform -chdir=terraform/${INPUT_TF_STAGE} init -backend-config="key=pipeline/${INPUT_STATE_KEY}-${INPUT_TF_STAGE}.tfstate"
-    terraform -chdir=terraform/${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
-    terraform -chdir=terraform/${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
-
+    terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}-stage2.tfstate"
+    terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
+    terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
 fi
