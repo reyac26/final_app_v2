f6cdfcd374a06657c23dde9e2e8974b0d0e29491
Reya <nereydacastro15@gmail.com>
2026-08-01 18:13:32 -0500

    Enhance entrypoint.sh for stage2 processing
    Add support for stage2 in Terraform entrypoint script.

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -10,4 +10,10 @@ if [[ "$TF_STAGE" == "stage1" ]]; then
     terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
     terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
     terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+
+elif [[ "$TF_STAGE" == "stage2" ]]; then
+    terraform -chdir=terraform/${INPUT_TF_STAGE} init -backend-config="key=pipeline/${INPUT_STATE_KEY}-${INPUT_TF_STAGE}.tfstate"
+    terraform -chdir=terraform/${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
+    terraform -chdir=terraform/${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+
 fi
