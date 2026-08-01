b3a3cd2e6fc160664e7d9f201458ea440ccaef96
Reya <nereydacastro15@gmail.com>
2026-08-01 15:27:24 -0500

    Add entrypoint script for Terraform GitHub Action

---
+++ .github/actions/terraform/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+export STATE_KEY=${INPUT_STATE_KEY}
+export TF_STAGE=${INPUT_TF_STAGE}
+export DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}
+
+if [[ "$TF_STAGE" == "stage1" ]]; then
+    terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
+    terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
+    terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+fi
