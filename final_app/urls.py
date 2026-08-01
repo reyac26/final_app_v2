e2f4c658f46c2f31f8d28b00724a90fc6393491f
Reya <nereydacastro15@gmail.com>
2026-08-01 16:06:25 -0500

    Add home smoke test view to URL patterns

--- final_app/urls.py
+++ final_app/urls.py
@@ -1,23 +1,18 @@
 """
 URL configuration for final_app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import include, path
 
+from django.http import HttpResponse
+
+
+def home_smoke_test_view(request):
+    return HttpResponse("Hello World")
+
 urlpatterns = [
-    path("", include("hello_final.urls")),
+    path("", home_smoke_test_view, name="root_smoke_test"),
+    
+    path("app/", include("hello_final.urls")),
     path('admin/', admin.site.urls),
 ]
