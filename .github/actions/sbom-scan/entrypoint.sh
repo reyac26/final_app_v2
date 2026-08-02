46cae13fc9f68adfff1446ba2202f08b07429516
Reya <nereydacastro15@gmail.com>
2026-08-01 20:47:10 -0500

    Enhance SBOM scan script with Syft and Grype
    Added steps for SBOM creation and vulnerability detection.

--- .github/actions/sbom-scan/entrypoint.sh
+++ .github/actions/sbom-scan/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+set -e
+cd /github/worspace
+
+echo "==== Step 1: Executing Custom SBOM Creation (Syft) ===="
 syft requirements.txt -o cyclonedx-json=sbom.json
+
+echo " ==== Step 2: Executing Custom Vulnerability Detection (Grype) ===="
 grype sbom.json --by-cve --fail-on critical
+
+echo "==== Security Audit Sweeps Completed Sucessfully ===="
