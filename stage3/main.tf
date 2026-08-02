1323ac4a913def3c7e381e6e5bfa3b0b1cde7784
Reya <nereydacastro15@gmail.com>
2026-08-01 21:43:14 -0500

    Set up Terraform with AWS provider and S3 backend
    Configure Terraform with required providers and S3 backend.

--- stage3/main.tf
+++ stage3/main.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_version = ">= 1.7.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" 
+    }
+  }
 
+  backend "s3" {
+    bucket  = "nereydacastro-final-state-bucket-v2"
+    key     = "stage3/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true 
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
